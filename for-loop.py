924dd87150b4ea57150cab58d01c5a149d518b4c
Ifanmz <91680459+Ifanmz@users.noreply.github.com>
2022-06-23 21:54:46 +0700

    for loop in python

---
+++ for-loop.py
@@ -0,0 +1,6 @@
+mahasiswa = ["Ifan","Rahma"]
+
+mahasiswa.append("Zaenudin")
+
+for nama in mahasiswa:
+    print("nama: ", nama)
--- list.py
+++ list.py
@@ -1,2 +1,21 @@
 # list type 
 
+# declaration
+nama = ["Ifan", "Rahma"]
+
+# add data to list
+nama.append("Rani")
+
+print(nama)
+
+# remove data from list
+nama.remove("Rani")
+
+print(nama)
+
+# edit list data
+nama.append("Rani")
+
+nama[2] = "Rania"
+
+print(nama)
