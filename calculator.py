95b9ec13699bc7a26c80895588d4dc8fb8fb28f4
Ifanmz <91680459+Ifanmz@users.noreply.github.com>
2022-08-04 09:58:21 +0700

    class on python

---
+++ calculator.py
@@ -0,0 +1,8 @@
+class Kalkulator:
+
+    def __init__(self, number):
+        self.number = number
+
+    def kali(self, num):
+        result = self.number * num
+        return result
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append('/home/dicoding')
 
 import hello
+import calculator
 
 hello.helloWorld()
 
@@ -10,3 +11,7 @@ wawa = hello.Reviewer("Wawa", "12")
 wawa.review()
 
 print(sys.path)
+
+number = calculator.Kalkulator(3)
+
+print("hasil kali dari 3 * 4 adalah: ", number.kali(4))
